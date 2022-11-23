4d29b4c55684bb06243b5d73b5c2799985ecdd4f
Yakov Popov <suburbanms@gmail.com>
2022-11-23 15:32:22 +0700

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.начатьЗановоToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.новаяИграToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сложностьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.легкоToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.нормальноToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сложноToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.завершитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.начатьЗановоToolStripMenuItem,
+            this.новаяИграToolStripMenuItem,
+            this.сложностьToolStripMenuItem,
+            this.завершитьToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(439, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // начатьЗановоToolStripMenuItem
+            // 
+            this.начатьЗановоToolStripMenuItem.Name = "начатьЗановоToolStripMenuItem";
+            this.начатьЗановоToolStripMenuItem.Size = new System.Drawing.Size(99, 20);
+            this.начатьЗановоToolStripMenuItem.Text = "Начать заново";
+            this.начатьЗановоToolStripMenuItem.Click += new System.EventHandler(this.начатьЗановоToolStripMenuItem_Click);
+            // 
+            // новаяИграToolStripMenuItem
+            // 
+            this.новаяИграToolStripMenuItem.Name = "новаяИграToolStripMenuItem";
+            this.новаяИграToolStripMenuItem.Size = new System.Drawing.Size(81, 20);
+            this.новаяИграToolStripMenuItem.Text = "Новая игра";
+            this.новаяИграToolStripMenuItem.Click += new System.EventHandler(this.новаяИграToolStripMenuItem_Click);
+            // 
+            // сложностьToolStripMenuItem
+            // 
+            this.сложностьToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.легкоToolStripMenuItem,
+            this.нормальноToolStripMenuItem,
+            this.сложноToolStripMenuItem});
+            this.сложностьToolStripMenuItem.Name = "сложностьToolStripMenuItem";
+            this.сложностьToolStripMenuItem.Size = new System.Drawing.Size(81, 20);
+            this.сложностьToolStripMenuItem.Text = "Сложность";
+            // 
+            // легкоToolStripMenuItem
+            // 
+            this.легкоToolStripMenuItem.Name = "легкоToolStripMenuItem";
+            this.легкоToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.легкоToolStripMenuItem.Text = "Легко";
+            this.легкоToolStripMenuItem.Click += new System.EventHandler(this.легкоToolStripMenuItem_Click);
+            // 
+            // нормальноToolStripMenuItem
+            // 
+            this.нормальноToolStripMenuItem.Name = "нормальноToolStripMenuItem";
+            this.нормальноToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.нормальноToolStripMenuItem.Text = "Нормально";
+            this.нормальноToolStripMenuItem.Click += new System.EventHandler(this.нормальноToolStripMenuItem_Click);
+            // 
+            // сложноToolStripMenuItem
+            // 
+            this.сложноToolStripMenuItem.Name = "сложноToolStripMenuItem";
+            this.сложноToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.сложноToolStripMenuItem.Text = "Сложно";
+            this.сложноToolStripMenuItem.Click += new System.EventHandler(this.сложноToolStripMenuItem_Click);
+            // 
+            // завершитьToolStripMenuItem
+            // 
+            this.завершитьToolStripMenuItem.Name = "завершитьToolStripMenuItem";
+            this.завершитьToolStripMenuItem.Size = new System.Drawing.Size(80, 20);
+            this.завершитьToolStripMenuItem.Text = "Завершить";
+            this.завершитьToolStripMenuItem.Click += new System.EventHandler(this.завершитьToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.panel1.Location = new System.Drawing.Point(0, 24);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(88, 254);
+            this.panel1.TabIndex = 0;
+            // 
+            // form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(439, 410);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(40, 40);
+            this.Name = "form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem начатьЗановоToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сложностьToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ToolStripMenuItem новаяИграToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem легкоToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem нормальноToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сложноToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem завершитьToolStripMenuItem;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public enum Level
+    {
+        easy = 0, //20%
+        normal = 1, //40%
+        hard = 2 //60%
+    }
+    
+
+    public partial class form1 : Form
+    {
+        public form1()
+        {
+            InitializeComponent();
+        }
+
+        Square btn = new Square();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            panel1.Dock = DockStyle.Fill;
+            btn.Build(panel1);
+            btn.SetMine(Level.easy);
+
+            foreach (var item in panel1.Controls) //обходим все элементы формы
+            {
+                if (item is Button) // проверяем, что это кнопка
+                {
+                    ((Button)item).Click += CommonBtn_Click; //приводим к типу и устанавливаем обработчик события  
+                    ((Button)item).DoubleClick += CommonBtn_DoubleClick;
+                }
+            }
+        }
+
+        private void CommonBtn_DoubleClick(object sender, EventArgs e)
+        {
+            /*Square item = (Square)sender;
+            if (item.flag == false)
+            {
+                item.flag = true;
+                item.BackgroundImage = Properties.Resources.image;
+                MessageBox.Show("131");
+            }
+            if (item.flag == true)
+            {
+                item.flag = false;
+                MessageBox.Show("131");
+            }*/
+        }
+
+        private void CommonBtn_Click(object sender, EventArgs e)
+        {
+            Square item = (Square)sender;
+            if (Square.checkedNow == 0)
+            {
+                btn.Timer(timer1, 0);
+            }
+            if (item.bomb == true)
+            {
+                btn.Loose();
+                MessageBox.Show(btn.Timer(timer1, 1), "Неудача");
+                panel1.Enabled = false;
+                return;
+            }
+            Square.checkedNow += 1;
+            item.Text = item.NumBomb(item);
+            item.BackColor = Color.DarkGray;
+        }
+
+        private void начатьЗановоToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            panel1.Enabled = true;
+            btn.ClearView();
+            btn.Timer(timer1, 1);
+        }
+
+        private void новаяИграToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            panel1.Enabled = true;
+            btn.ClearView();
+            btn.ClearMine();
+            btn.SetMine(Level.easy);
+            btn.Timer(timer1, 1);
+        }
+
+        private void легкоToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            panel1.Enabled = true;
+            btn.ClearView();
+            btn.ClearMine();
+            btn.SetMine(Level.easy);
+            btn.Timer(timer1, 1);
+        }
+
+        private void нормальноToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            panel1.Enabled = true;
+            btn.ClearView();
+            btn.ClearMine();
+            btn.SetMine(Level.normal);
+            btn.Timer(timer1, 1);
+        }
+
+        private void сложноToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            panel1.Enabled = true;
+            btn.ClearView();
+            btn.ClearMine();
+            btn.SetMine(Level.hard);
+            btn.Timer(timer1, 1);
+        }
+
+        private void завершитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (Square.checkedNow == 0)
+            {
+                MessageBox.Show("Проверьте хотя бы одно поле", "Оповещение");
+                return;
+            }
+            panel1.Enabled = false;
+
+            if (Square.checkedToWin == Square.checkedNow)
+            {
+                MessageBox.Show(btn.Timer(timer1, 1), "Победа");
+            }
+            else
+            {
+                MessageBox.Show(btn.Timer(timer1, 1), "Неудача");
+            }
+            
+        }
+    }
+
+
+
+    class Square : Button
+    {
+        DateTime dateTimeStart, dateTimeEnd;
+        static int size_x = 10;
+        static int size_y = 10;
+        static int count = size_x * size_y;
+        public bool flag = false;
+        public bool bomb = false;
+        static public int checkedToWin;
+        static public int checkedNow = 0;
+        int start_x = 0;
+        int start_y = 0;
+        const int ButtonWidth = 40;
+        const int ButtonHeight = 40;
+        
+
+        public static Square[,] btnArr = new Square[size_x, size_y];
+
+        public Square()
+        {
+
+        }
+
+        public void Build(Panel panel)
+        {
+            int counter = 0;
+            for (int x = 0; x < size_x; x++)
+            {
+                for (int y = 0; y < size_y; y++)
+                {
+                    btnArr[x, y] = new Square();
+                    btnArr[x, y].Name = counter.ToString();
+                    btnArr[x, y].BackColor = Color.Transparent;
+                    btnArr[x, y].Top = start_x + (x * ButtonHeight);
+                    btnArr[x, y].Left = start_y + (y * ButtonWidth);
+                    btnArr[x, y].Width = ButtonWidth;
+                    btnArr[x, y].Height = ButtonHeight;
+
+                    panel.Controls.Add(btnArr[x, y]);
+                    ++counter;
+                }
+
+            }
+        }
+
+        public void SetMine(Level level)
+        {
+            int tempCol, tempRow;
+            double tempLvl = 0.2;
+            if (level == Level.easy)
+            {
+                tempLvl = 0.2;
+            }
+            if (level == Level.normal)
+            {
+                tempLvl = 0.4;
+
+            }
+            if (level == Level.hard)
+            {
+                tempLvl = 0.6;
+
+            }
+
+            int limit = (int)Math.Round(count * tempLvl);
+            checkedToWin = count - limit;
+
+            Random rnd = new Random();
+            for (int i = 0; i != limit;)
+            {
+                tempCol = rnd.Next(0, size_y);
+                tempRow = rnd.Next(0, size_x);
+                if (btnArr[tempRow, tempCol].bomb == false)
+                {
+                    btnArr[tempRow, tempCol].bomb = true;
+                    ++i;
+                }
+            }
+
+        }
+        public string NumBomb(Square item)
+        {
+            int number = Convert.ToInt32(item.Name);
+            int result = 0;
+            int col = number % size_y - 2;
+            int temp = col;
+            int row = (number / size_x) - 2;
+            for (int i = 0; i < 3; i++)
+            {
+                row += 1;
+
+                col = temp;
+                for (int j = 0; j < 3; j++)
+                {
+                    col += 1;
+                    if (col < 0 || col > (size_y - 1) || row < 0 || row > (size_x - 1))
+                    {
+                        continue;
+                    }
+
+                    if (btnArr[row, col].bomb != false)
+                    {
+                        result += 1;
+                    }
+
+                }
+            }
+            return $"{result}";
+        }
+
+        public void ClearView()
+        {
+            for (int x = 0; x < size_x; x++)
+            {
+                for (int y = 0; y < size_y; y++)
+                {
+                    btnArr[x, y].BackColor = Color.Transparent;
+                    btnArr[x, y].BackgroundImage = null;
+                    btnArr[x, y].Text = null;
+                    checkedNow = 0;
+                }
+
+            }
+        }
+
+        public void ClearMine()
+        {
+            for (int x = 0; x < size_x; x++)
+            {
+                for (int y = 0; y < size_y; y++)
+                {
+                    btnArr[x, y].bomb = false;
+                }
+
+            }
+        }
+
+        public void Loose()
+        {
+            for (int x = 0; x < size_x; x++)
+            {
+                for (int y = 0; y < size_y; y++)
+                {
+                    if (btnArr[x, y].bomb == true)
+                    {
+                        btnArr[x, y].BackgroundImage = Properties.Resources.bomb;
+                        btnArr[x, y].BackgroundImageLayout = ImageLayout.Center;
+                    }
+                }
+
+            }
+        }
+
+        public string Timer(Timer timer, int type)
+        {
+            string result = "";
+
+            if (type == 0)
+            {
+                timer.Start();
+                dateTimeStart = DateTime.Now;
+            } 
+            else if (type == 1)
+            {
+                dateTimeEnd = DateTime.Now;
+                TimeSpan temp = dateTimeEnd - dateTimeStart;
+                timer.Stop();
+                result = "Время: " + temp.Minutes.ToString() + ":" + temp.Seconds.ToString() + ":" + temp.Milliseconds.ToString();
+            }
+
+            return result;
+        }
+
+    }
+}
